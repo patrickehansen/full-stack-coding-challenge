4cfe149d9e00d190d354ff6ef9944673b362cf0b
Tiskahar <tiskahar@adiclan.com>
2021-09-16 21:48:52 -0500

    clean up the dictionary generation to prevent it from being called too frequently

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,28 @@ import useApiData from '../hooks/use-api-data'
 import Airport from '../types/airport'
 import AirportDictionary from '../types/airportDictionary'
 
+let airportDictionary;
+
+// Minimal implementation to cache this response so we only have to do it once.
+function mapAirports (airports): AirportDictionary {
+  if (airportDictionary) return airportDictionary;
+
+  airportDictionary = airports.reduce((a, v) => {
+    a[v.name] = v;
+    a[v.iata] = v;
+    a[v.city] = v;
+    a[v.country] = v;
+
+    return a;
+  }, {});
+
+  return airportDictionary;
+}
+
 const Page: NextPage = () => {
   const airports = useApiData<Airport[]>('/api/airports', []);
 
   const [ filteredAirports, setFiltered ] = useState([])
-
-  function mapAirports (airports): AirportDictionary {
-    return airports.reduce((a, v) => {
-      a[v.name] = v;
-      a[v.iata] = v;
-      a[v.city] = v;
-      a[v.country] = v;
-
-      return a;
-    }, {});
-  }
 
   return <Layout>
     <h1 className='text-2xl font-bold'>Code Challenge: Airports</h1>
